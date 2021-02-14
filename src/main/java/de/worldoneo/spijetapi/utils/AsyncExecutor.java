0d6603213f1e884917fd835b8a6da3ac1fedd731
worldOneo <62426504+worldOneo@users.noreply.github.com>
2021-02-14 15:28:54 +0100

    Add AsyncExecutor#complete -> CompletableFuture Ref ItemStackBuilder

--- src/main/java/de/worldoneo/spijetapi/utils/AsyncExecutor.java
+++ src/main/java/de/worldoneo/spijetapi/utils/AsyncExecutor.java
@@ -1,9 +1,7 @@
 package de.worldoneo.spijetapi.utils;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
+import java.util.function.Supplier;
 
 public class AsyncExecutor {
     private final ThreadPoolExecutor threadPoolExecutor;
@@ -16,8 +14,28 @@ public class AsyncExecutor {
         threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(corePoolSize);
     }
 
+    /**
+     * Runs the callable async and returns its future.
+     * Its recommended to take a look at <code>#complete(Supplier)</code>
+     * as it provides better handling.
+     *
+     * @param callable the callable to run async
+     * @param <T>      the type which the callable returns
+     * @return the future of the async running callable
+     */
     public <T> Future<T> submit(Callable<T> callable) {
         return getThreadPoolExecutor().submit(callable);
+    }
+
+    /**
+     * Runs the supplier async and returns a {@link CompletableFuture}
+     *
+     * @param supplier the supplier to run in the completable future
+     * @param <T>      the return type of the supplier
+     * @return a {@link CompletableFuture} executing the task
+     */
+    public <T> CompletableFuture<T> complete(Supplier<T> supplier) {
+        return CompletableFuture.supplyAsync(supplier, getThreadPoolExecutor());
     }
 
     public void submit(Runnable runnable) {
