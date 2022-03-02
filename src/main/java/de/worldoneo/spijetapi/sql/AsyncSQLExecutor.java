ed2f44e7888e7dc454364da74628c9552168be25
NoJokeFNA <NoJokeFNA@gmail.com>
2022-03-02 09:18:56 +0100

    Ye interface :flame:

--- src/main/java/de/worldoneo/spijetapi/sql/AsyncSQLExecutor.java
+++ src/main/java/de/worldoneo/spijetapi/sql/AsyncSQLExecutor.java
@@ -6,5 +6,6 @@ import java.util.concurrent.CompletableFuture;
 
 public interface AsyncSQLExecutor<T> {
     CompletableFuture<CachedRowSet> executeUpdateAsync(T arg) throws SQLException;
+
     CompletableFuture<CachedRowSet> executeQueryAsync(T arg) throws SQLException;
 }
